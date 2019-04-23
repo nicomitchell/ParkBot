540a924ed5e46888bd361e705932457bdbfcefa8
Nico Mitchell <namitc02@louisville.edu>
2019-04-22 21:38:38 -0400

    removed lines in csv file

--- csv_cleanup.go
+++ csv_cleanup.go
@@ -3,20 +3,30 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
 	r, err := os.Open("VEHICLES_NOPERMIT_CLEANED.csv")
 	if err == nil {
-		readFromCSVFile(bufio.NewReader(r))
-		//var objs []licenseObj
-		//objs, err = readFromCSVFile(bufio.NewReader(r))
-		// for _, v := range objs {
-		// 	fmt.Println(v.state + " " + v.plateNumber + "\t" + string(v.vehicleUID))
-		// 	fmt.Println("\t" + strings.Join(v.permits, " "))
-		// }
+		// readFromCSVFile(bufio.NewReader(r))
+		var objs []*licenseObj
+		objs, err = readFromCSVFile(bufio.NewReader(r))
+		if err == nil {
+			fmt.Println(len(objs))
+			for _, v := range objs {
+				fmt.Println(v.state + " " + v.plateNumber + "\t" + strconv.Itoa(v.vehicleUID))
+				for _, p := range v.permits {
+					fmt.Println("\t\t\t" + p)
+				}
+			}
+		} else {
+			fmt.Println(err.Error())
+		}
+		r.Close()
 	}
 }
 
@@ -27,16 +37,46 @@ type licenseObj struct {
 	permits     []string
 }
 
-func readFromCSVFile(r *bufio.Reader) { //([]licenseObj, error) {
+func readFromCSVFile(r *bufio.Reader) ([]*licenseObj, error) {
+	var out []*licenseObj
+	line, err := r.ReadBytes('\n')
 	for {
-		line, err := r.ReadBytes('\n')
 		if err == nil {
 			values := getValuesFromLine(string(line))
-			fmt.Println(len(values), "\t", strings.Join(values, ", "))
-		} else {
-			return
+			var vuid int
+			vuid, err = strconv.Atoi(values[0])
+			if err == nil {
+				obj := &licenseObj{
+					vehicleUID:  vuid,
+					state:       values[1][:2],
+					plateNumber: values[1][3:],
+					permits:     []string{},
+				}
+				for {
+					line, err = r.ReadBytes('\n')
+					if err == nil {
+						values = getValuesFromLine(string(line))
+						if len(values) > 1 {
+							break
+						} else {
+							obj.permits = append(obj.permits, values[0])
+						}
+					} else {
+						break
+					}
+				}
+				if err == nil {
+					out = append(out, obj)
+					continue
+				}
+			}
 		}
+		if err != io.EOF {
+			return nil, err
+		}
+		break
 	}
+	return out, nil
 }
 
 func getValuesFromLine(line string) []string {
@@ -44,11 +84,9 @@ func getValuesFromLine(line string) []string {
 	for strings.Contains(line, ",,") {
 		line = strings.Replace(line, ",,", ",", -1)
 	}
-	fmt.Println(line)
 	split := strings.Split(line, ",")
 	if len(split) == 1 {
 		return split
 	}
 	return split[:2]
-
 }
