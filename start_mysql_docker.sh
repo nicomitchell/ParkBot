19590e6289830125f5deb1cf891f494cb1cd6fdd
NicoMitchell <nicolasm@ads.isi.edu>
2019-04-28 20:25:22 -0400

    added env variables and docker argument to start script

--- start_mysql_docker.sh
+++ start_mysql_docker.sh
@@ -2,4 +2,4 @@
 
 sudo docker pull mysql/mysql-server:latest
 
-sudo docker run mysql/mysql-server:latest
+sudo docker run -dit -p 3306:3306 --name=parkbot_db -e MYSQL_DATABASE=parkbot -e MYSQL_ROOT_PASSWORD=test-pw mysql/mysql-server:latest
