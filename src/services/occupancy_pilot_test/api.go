8556176f93e7267bac23827fe7d25695316336c2
Nico Mitchell <namitc02@louisville.edu>
2019-05-26 23:23:28 -0400

    occupancy function

--- src/services/occupancy_pilot_test/api.go
+++ src/services/occupancy_pilot_test/api.go
@@ -41,3 +41,47 @@ func (sh *OccupancyStatusHandler) GetLotInfo(w http.ResponseWriter, r *http.Requ
 		}
 	}
 }
+
+//NewLot creates a new lot
+func (sh *OccupancyStatusHandler) NewLot(w http.ResponseWriter, r *http.Request) {
+	d := r.Body
+	data := []byte{}
+	_, err := d.Read(data)
+	if err == nil {
+		lot, err := decodeLot(data)
+		if err == nil {
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(lot)
+			return
+		}
+	}
+	w.WriteHeader(http.StatusBadRequest)
+	json.NewEncoder(w).Encode(errMessage{Message: err.Error()})
+}
+
+//GetOccupancyInformation gets occupancy info for the lot given
+func (sh *OccupancyStatusHandler) GetOccupancyInformation(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	if v, ok := vars["lot"]; !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(errMessage{Message: "you must include a lot id"})
+	} else {
+		if l, ok := sh.lots[v]; !ok {
+			w.WriteHeader(http.StatusNotFound)
+			json.NewEncoder(w).Encode(errMessage{Message: "no lot matching that id found"})
+		} else {
+			type resp struct {
+				OpenSpots   int     `json:"open_spots"`
+				Occupied    int     `json:"occupied_spots"`
+				PercentFull float64 `json:"percent_full"`
+			}
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(
+				resp{
+					OpenSpots:   l.MaxOccupancy - l.Occupancy(),
+					Occupied:    l.Occupancy(),
+					PercentFull: l.PercentFull(),
+				})
+		}
+	}
+}
