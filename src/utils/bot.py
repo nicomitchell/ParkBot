83522522b7f16843828a200fbee7451f9dc40aef
Nicolas Mitchell <namitc02@louisville.edu>
2019-07-16 14:33:53 -0400

    setup

--- src/utils/bot.py
+++ src/utils/bot.py
@@ -1,6 +1,7 @@
 import tweepy
 import os
 from random import randint
+from time import sleep
 consumer_key = os.environ["TWITTER_API_KEY"]
 consumer_secret = os.environ["TWITTER_API_SECRET"]
 access_token = os.environ["TWITTER_ACCESS_TOKEN"]
@@ -11,13 +12,17 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-geolocation = "38.215311,-85.758189,300mi"
-tweet_candidates = api.search(
-    q="filter:safe \"can't find parking\" OR \"trying to find parking\" -filter:retweets -filter:links -filter:images",
-    geolocation=geolocation,
-    result_type="recent",
-    count=100,
-    language="eng",
-    tweet_mode="extended")
+while(True):
+    geolocation = "38.215311,-85.758189,300mi"
+    tweet_candidates = api.search(
+        q="filter:safe \"can't find parking\" OR \"trying to find parking\" -filter:retweets -filter:links -filter:images",
+        geolocation=geolocation,
+        result_type="recent",
+        count=100,
+        language="eng",
+        tweet_mode="extended")
 
-s = api.create_favorite(tweet_candidates[randint(0,len(tweet_candidates)-1)].id)
+    s = api.create_favorite(tweet_candidates[randint(0,len(tweet_candidates)-1)].id)
+    if str(input("continue? ")=="n"):
+        break
+    sleep(randint(3800,10000))
